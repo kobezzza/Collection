2b43004ba41f45b4fee3f03d696d9745985132ff
kobezzza <kobezzza@gmail.com>
2016-07-11 14:16:14 +0300

    Added compiled build for node

---
+++ compiled.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*!
+ * Collection
+ * https://github.com/kobezzza/Collection
+ *
+ * Released under the MIT license
+ * https://github.com/kobezzza/Collection/blob/master/LICENSE
+ */
+
+module.exports = require('./dist/collection.min');
