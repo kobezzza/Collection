f8a1ee4a8c1dd4c33b374a5db43d0649cb71e8aa
kobezzza <kobezzza@gmail.com>
2016-07-13 11:53:30 +0300

    :wrench:

---
+++ dist/node/iterators/helpers.js
@@ -0,0 +1,140 @@
+'use strict';
+
+/*!
+ * Collection
+ * https://github.com/kobezzza/Collection
+ *
+ * Released under the MIT license
+ * https://github.com/kobezzza/Collection/blob/master/LICENSE
+ */
+
+var _core = require('../core');
+
+var _types = require('../helpers/types');
+
+var _gcc = require('../helpers/gcc');
+
+/**
+ * Appends a filter to the operation
+ *
+ * @param {...($$CollectionFilter|Array<$$CollectionFilter>|undefined)} filter - function filter
+ * @return {!Collection}
+ */
+_core.Collection.prototype.filter = function (filter) {
+	let args = [];
+	for (let i = 0; i < arguments.length; i++) {
+		const el = arguments[i];
+
+		if (el) {
+			args = args.concat(el);
+		}
+	}
+
+	this.p.filter = this.p.filter.concat.apply(this.p.filter, args);
+	return this;
+};
+
+/**
+ * Marks the operation as thread
+ *
+ * @param {(?string|$$CollectionThreadCb)=} [opt_priority] - thread priority (low, normal, hight, critical)
+ * @param {?$$CollectionThreadCb=} [opt_onChunk] - callback function for chunks
+ * @return {!Collection}
+ */
+_core.Collection.prototype.thread = function (opt_priority, opt_onChunk) {
+	if ((0, _types.isFunction)(opt_priority)) {
+		opt_onChunk = (0, _gcc.any)(opt_priority);
+		opt_priority = null;
+	}
+
+	this.p.thread = true;
+
+	if (opt_priority) {
+		this.p.priority = opt_priority;
+	}
+
+	if (opt_onChunk) {
+		this.p.onChunk = opt_onChunk;
+	}
+
+	return this;
+};
+
+/**
+ * Sets .startIndex for the operation
+ *
+ * @param {number} value - source value
+ * @return {!Collection}
+ */
+_core.Collection.prototype.start = function (value) {
+	this.p.startIndex = value;
+	return this;
+};
+
+/**
+ * Sets .endIndex for the operation
+ *
+ * @param {number} value - source value
+ * @return {!Collection}
+ */
+_core.Collection.prototype.end = function (value) {
+	this.p.endIndex = value;
+	return this;
+};
+
+/**
+ * Sets .from for the operation
+ *
+ * @param {number} value - source value
+ * @return {!Collection}
+ */
+_core.Collection.prototype.from = function (value) {
+	this.p.from = value;
+	return this;
+};
+
+/**
+ * Sets .count for the operation
+ *
+ * @param {number} value - source value
+ * @return {!Collection}
+ */
+_core.Collection.prototype.count = function (value) {
+	this.p.count = value;
+	return this;
+};
+
+Object.defineProperties(_core.Collection.prototype, /** @lends {Collection.prototype} */{
+	one: {
+		/**
+   * Sets .mult to false for the operation
+   * @return {!Collection}
+   */
+		get() {
+			this.p.mult = false;
+			return this;
+		}
+	},
+
+	inverse: {
+		/**
+   * Sets .inverseFilter to true for the operation
+   * @return {!Collection}
+   */
+		get() {
+			this.p.inverseFilter = true;
+			return this;
+		}
+	},
+
+	reverse: {
+		/**
+   * Sets .reverse to true for the operation
+   * @return {!Collection}
+   */
+		get() {
+			this.p.reverse = true;
+			return this;
+		}
+	}
+});
