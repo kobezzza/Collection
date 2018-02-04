8889763b7431c0b65174c5716837d3e7794583ff
kobezzza <kobezzza@gmail.com>
2018-02-04 17:52:53 +0300

    :new: dev:node, watch:node

--- gulpfile.js
+++ gulpfile.js
@@ -38,8 +38,24 @@ gulp.task('dev', gulp.parallel([
 	'npmignore'
 ]));
 
+gulp.task('dev:node', gulp.parallel([
+	'copyright',
+	'head',
+	'build:node',
+	'bump',
+	'yaspeller',
+	'npmignore'
+]));
+
 gulp.task('watch', gulp.series(['dev', () => {
 	gulp.watch('./src/**/*.js', gulp.parallel(['build:client', 'build:node']));
+	gulp.watch('./src/core.js', gulp.series('bump'));
+	gulp.watch('./*.md', gulp.series('yaspeller'));
+	gulp.watch('./.gitignore', gulp.series('npmignore'));
+}]));
+
+gulp.task('watch:node', gulp.series(['dev:node', () => {
+	gulp.watch('./src/**/*.js', gulp.parallel(['build:node']));
 	gulp.watch('./src/core.js', gulp.series('bump'));
 	gulp.watch('./*.md', gulp.series('yaspeller'));
 	gulp.watch('./.gitignore', gulp.series('npmignore'));
