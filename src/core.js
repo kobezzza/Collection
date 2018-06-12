000538e26bb4f96a687dee136ec881e154d79cc2
kobezzza <kobezzza@gmail.com>
2018-06-12 22:31:40 +0300

    :arrow_up:

--- src/core.js
+++ src/core.js
@@ -90,7 +90,7 @@ Object.assign($C, {config: {}});
  * Library version
  * @const
  */
-Collection.prototype.VERSION = [6, 6, 24];
+Collection.prototype.VERSION = [6, 6, 25];
 
 /**
  * Creates an instance of Collection
