d52c6318fe5e3ca8553a233d089b667035b3e92c
kobezzza <kobezzza@gmail.com>
2019-04-25 12:31:42 +0300

    Added spec boilerplate

---
+++ spec/tests.js
@@ -0,0 +1,11 @@
+/*!
+ * Collection
+ * https://github.com/kobezzza/Collection
+ *
+ * Released under the MIT license
+ * https://github.com/kobezzza/Collection/blob/master/LICENSE
+ */
+
+describe('Collection', () => {
+
+});
