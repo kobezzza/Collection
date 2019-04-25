1bcba12ac29be6dbf306c189f634086f90ec82ab
kobezzza <kobezzza@gmail.com>
2019-04-25 15:47:50 +0300

    Added gulp tasks for tests

--- gulp/test.js
+++ gulp/test.js
@@ -12,5 +12,31 @@ const
 	gulp = require('gulp'),
 	$ = require('gulp-load-plugins')();
 
-gulp.task('test', () => $.run('node test').exec().on('error', console.error));
+function test() {
+	const
+		dev = arguments[0] === true;
+
+	function exec() {
+		return gulp.src(`./dist/collection${!dev ? '.min' : ''}.js`)
+			.pipe($.istanbul())
+			.pipe($.istanbul.hookRequire())
+			.on('finish', runTests);
+
+		function runTests() {
+			return gulp.src(`./spec/${dev ? 'dev' : 'index'}_spec.js`)
+				.pipe($.plumber())
+				.pipe($.jasmine())
+				.pipe($.istanbul.writeReports());
+		}
+	}
+
+	if (dev) {
+		return exec;
+	}
+
+	return exec();
+}
+
+gulp.task('test', test);
+gulp.task('test:dev', gulp.series(['build:browser', test(true)]));
 gulp.task('yaspeller', () => $.run('yaspeller ./').exec().on('error', console.error));
