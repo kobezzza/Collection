4a4e850e69ad442ca95d20f20cc4fbc01ba926ba
kobezzza <kobezzza@gmail.com>
2019-05-14 08:51:56 +0300

    Fixed race with babel and bump tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,6 @@
  * https://github.com/kobezzza/Collection/blob/master/LICENSE
  */
 
-global.readyToWatcher = null;
-
 const
 	gulp = require('gulp');
 
@@ -20,43 +18,51 @@ require('./gulp/build');
 require('./gulp/test');
 
 gulp.task('default', gulp.parallel([
+	gulp.series([
+		gulp.parallel(['bump', 'head']),
+		'build'
+	]),
+
 	'copyright',
-	'head',
-	'build',
-	'bump',
 	'yaspeller',
 	'npmignore'
 ]));
 
 gulp.task('dev', gulp.parallel([
+	gulp.series([
+		gulp.parallel(['bump', 'head']),
+		'build:node',
+		'build:browser'
+	]),
+
 	'copyright',
-	'head',
-	'build:node',
-	'build:browser',
-	'bump',
 	'yaspeller',
 	'npmignore'
 ]));
 
 gulp.task('dev:node', gulp.parallel([
+	gulp.series([
+		gulp.parallel(['bump', 'head']),
+		'build:node'
+	]),
+
 	'copyright',
-	'head',
-	'build:node',
-	'bump',
 	'yaspeller',
 	'npmignore'
 ]));
 
 gulp.task('watch', gulp.series(['dev', () => {
-	gulp.watch('./src/**/*.js', gulp.parallel(['build:browser', 'build:node']));
-	gulp.watch('./src/core.js', gulp.series('bump'));
+	gulp.watch('./src/**/*.js', gulp.series([
+		'bump',
+		gulp.parallel(['build:browser', 'build:node'])
+	]));
+
 	gulp.watch('./*.md', gulp.series('yaspeller'));
 	gulp.watch('./.gitignore', gulp.series('npmignore'));
 }]));
 
 gulp.task('watch:node', gulp.series(['dev:node', () => {
-	gulp.watch('./src/**/*.js', gulp.parallel(['build:node']));
-	gulp.watch('./src/core.js', gulp.series('bump'));
+	gulp.watch('./src/**/*.js', gulp.series(['bump', 'build:node']));
 	gulp.watch('./*.md', gulp.series('yaspeller'));
 	gulp.watch('./.gitignore', gulp.series('npmignore'));
 }]));
