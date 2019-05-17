300dfa35648ec67644fa59d80402cb4defae356c
kobezzza <kobezzza@gmail.com>
2019-05-17 19:59:22 +0300

    Added more cases

---
+++ spec/cases/map.js
@@ -0,0 +1,389 @@
+/*!
+ * Collection
+ * https://github.com/kobezzza/Collection
+ *
+ * Released under the MIT license
+ * https://github.com/kobezzza/Collection/blob/master/LICENSE
+ */
+
+describe('Collection.map', () => {
+	it('map with a number', () => {
+		expect($C(3).map((el, i) => (i + 1) * 2)).toEqual([2, 4, 6]);
+	});
+
+	it('map with a string', () => {
+		expect($C('123').map((el) => el * 2)).toEqual([2, 4, 6]);
+	});
+
+	it('map with an array', () => {
+		expect($C([1, 2, 3]).map((el) => el * 2)).toEqual([2, 4, 6]);
+	});
+
+	it('map with an array like', () => {
+		expect($C({0: 1, 1: 2, 2: 3, length: 3}).map((el) => el * 2)).toEqual({0: 2, 1: 4, 2: 6, length: 3});
+	});
+
+	it('map with a filter and an array', () => {
+		expect($C([1, 2, 3]).filter((el) => el > 1).map((el) => el * 2)).toEqual([4, 6]);
+		expect($C([1, 2, 3]).map((el) => el * 2, (el) => el > 1)).toEqual([4, 6]);
+		expect($C([1, 2, 3]).map((el) => el * 2, [(el) => el > 1, (el) => el > 2])).toEqual([6]);
+	});
+
+	it('map with a filter and without a map function', () => {
+		expect($C([1, 2, 3]).filter((el) => el > 1).map()).toEqual([2, 3]);
+	});
+
+	it('reversed map with an array', () => {
+		expect($C([1, 2, 3]).reverse.map((el) => el * 2)).toEqual([6, 4, 2]);
+	});
+
+	it('map with an array and bounds', () => {
+		const
+			data = [1, 2, 3, 4, 5, 6];
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.map((el) => el * 2)
+		).toEqual([6, 8, 10]);
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual([6, 8]);
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.live
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual([6, 8]);
+
+		expect(
+			$C(data)
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual([8, 6]);
+	});
+
+	it('async map with an array and bounds', async () => {
+		const
+			data = [1, 2, 3, 4, 5, 6];
+
+		expect(
+			await $C(data)
+				.async
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => Promise.resolve(el * 2))
+		).toEqual([8, 6]);
+	});
+
+	it('map with an object', () => {
+		expect($C({a: 1, b: 2, c: 3}).map((el) => el * 2)).toEqual({a: 2, b: 4, c: 6});
+	});
+
+	it('map with a filter and an object', () => {
+		expect($C({a: 1, b: 2, c: 3}).filter((el) => el > 1).map((el) => el * 2)).toEqual({b: 4, c: 6});
+	});
+
+	it('map with an object and bounds', () => {
+		const
+			data = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6};
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.map((el) => el * 2)
+		).toEqual({c: 6, d: 8, e: 10});
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual({c: 6, d: 8});
+	});
+
+	it('async map with an object and bounds', async () => {
+		const
+			data = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6};
+
+		expect(
+			await $C(data)
+				.async
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => Promise.resolve(el * 2))
+		).toEqual({c: 6, d: 8});
+	});
+
+	it('map with a set', () => {
+		expect($C(new Set([1, 2, 3])).map((el) => el * 2)).toEqual(new Set([2, 4, 6]));
+	});
+
+	it('map with a filter and a set', () => {
+		expect($C(new Set([1, 2, 3])).filter((el) => el > 1).map((el) => el * 2)).toEqual(new Set([4, 6]));
+	});
+
+	it('reversed map with a set', () => {
+		expect($C(new Set([1, 2, 3])).reverse.map((el) => el * 2)).toEqual(new Set([6, 4, 2]));
+	});
+
+	it('map with a set and bounds', () => {
+		const
+			data = new Set([1, 2, 3, 4, 5, 6]);
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.map((el) => el * 2)
+		).toEqual(new Set([6, 8, 10]));
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual(new Set([6, 8]));
+
+		expect(
+			$C(data)
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual(new Set([8, 6]));
+	});
+
+	it('async map with a set and bounds', async () => {
+		const
+			data = new Set([1, 2, 3, 4, 5, 6]);
+
+		expect(
+			await $C(data)
+				.async
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => Promise.resolve(el * 2))
+		).toEqual(new Set([8, 6]));
+	});
+
+	it('map with a map', () => {
+		expect($C(new Map([['a', 1], ['b', 2], ['c', 3]])).map((el) => el * 2)).toEqual(new Map([['a', 2], ['b', 4], ['c', 6]]));
+	});
+
+	it('map with a filter and a map', () => {
+		expect($C(new Map([['a', 1], ['b', 2], ['c', 3]])).filter((el) => el > 1).map((el) => el * 2))
+			.toEqual(new Map([['b', 4], ['c', 6]]));
+	});
+
+	it('reversed map with a map', () => {
+		expect($C(new Map([['a', 1], ['b', 2], ['c', 3]])).reverse.map((el) => el * 2)).toEqual(new Map([['c', 6], ['b', 4], ['a', 2]]));
+	});
+
+	it('map with a map and bounds', () => {
+		const
+			data = new Map([['a', 1], ['b', 2], ['c', 3], ['d', 4], ['e', 5], ['f', 6]]);
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.map((el) => el * 2)
+		).toEqual(new Map([['c', 6], ['d', 8], ['e', 10]]));
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual(new Map([['c', 6], ['d', 8]]));
+
+		expect(
+			$C(data)
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual(new Map([['d', 8], ['c', 6]]));
+	});
+
+	it('async map with a map and bounds', async () => {
+		const
+			data = new Map([['a', 1], ['b', 2], ['c', 3], ['d', 4], ['e', 5], ['f', 6]]);
+
+		expect(
+			await $C(data)
+				.async
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => Promise.resolve(el * 2))
+		).toEqual(new Map([['d', 8], ['c', 6]]));
+	});
+
+	it('map with a generator', () => {
+		function *data() {
+			for (let i = 1; i <= 3; i++) {
+				yield i;
+			}
+		}
+
+		expect($C(data).map((el) => el * 2)).toEqual([2, 4, 6]);
+	});
+
+	it('map with a filter and a generator', () => {
+		function *data() {
+			for (let i = 1; i <= 3; i++) {
+				yield i;
+			}
+		}
+
+		expect($C(data).filter((el) => el > 1).map((el) => el * 2)).toEqual([4, 6]);
+	});
+
+	it('reversed map with a generator', () => {
+		function *data() {
+			for (let i = 1; i <= 3; i++) {
+				yield i;
+			}
+		}
+
+		expect($C(data).reverse.map((el) => el * 2)).toEqual([6, 4, 2]);
+	});
+
+	it('map with a generator and bounds', () => {
+		function *data() {
+			for (let i = 1; i <= 6; i++) {
+				yield i;
+			}
+		}
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.map((el) => el * 2)
+		).toEqual([6, 8, 10]);
+
+		expect(
+			$C(data)
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual([6, 8]);
+
+		expect(
+			$C(data)
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => el * 2)
+		).toEqual([8, 6]);
+	});
+
+	it('async map with a generator and bounds', async () => {
+		function *data() {
+			for (let i = 1; i <= 6; i++) {
+				yield i;
+			}
+		}
+
+		expect(
+			await $C(data)
+				.async
+				.reverse
+				.filter((el) => el > 1)
+				.start(1)
+				.from(1)
+				.count(3)
+				.end(3)
+				.map((el) => Promise.resolve(el * 2))
+		).toEqual([8, 6]);
+	});
+
+	it('map with an iterator', () => {
+		function *foo() {
+			for (let i = 1; i <= 3; i++) {
+				yield i;
+			}
+		}
+
+		expect($C(foo()).map((el) => el * 2)).toEqual([2, 4, 6]);
+	});
+
+	it('map with custom initial value', () => {
+		expect($C([1, 2, 3]).to(0).map()).toBe(6);
+		expect($C([1, 2, 3]).to('').map()).toBe('123');
+		expect($C([1, 2, 3]).to({}).map()).toEqual({0: 1, 1: 2, 2: 3});
+		expect($C([1, 2, 3]).to(new Set()).map()).toEqual(new Set([1, 2, 3]));
+		expect($C([1, 2, 3]).to(new Map()).map()).toEqual(new Map([[0, 1], [1, 2], [2, 3]]));
+	});
+
+	it('async map with custom initial value', async () => {
+		expect(await $C([1, 2, 3]).async.to(0).map((el) => Promise.resolve(el))).toBe(6);
+	});
+});
--- spec/cases/stream.js
+++ spec/cases/stream.js
@@ -14,7 +14,7 @@ describe('Collection methods with streams', () => {
 	const
 		{Transform} = require('stream');
 
-	function generateStream(i, method = 'push') {
+	function generateStream(i, error = false, method = 'push') {
 		const stream = new Transform({
 			readableObjectMode: true,
 			writableObjectMode: true,
@@ -29,6 +29,11 @@ describe('Collection methods with streams', () => {
 		(function loop() {
 			setTimeout(() => {
 				if (i--) {
+					if (error && j) {
+						stream.destroy(error);
+						return;
+					}
+
 					stream[method](j++);
 					loop();
 
@@ -60,12 +65,255 @@ describe('Collection methods with streams', () => {
 			});
 	});
 
+	it('async map from a stream', async (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy();
+
+		$C(generateStream(length)).async.map((el) => Promise.resolve(el * 2))
+			.addListener('data', dataHandler)
+			.addListener('end', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(length);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+				expect(dataHandler).toHaveBeenCalledWith(2);
+				expect(dataHandler).toHaveBeenCalledWith(4);
+				expect(dataHandler).toHaveBeenCalledWith(6);
+				expect(dataHandler).toHaveBeenCalledWith(8);
+				expect(dataHandler).not.toHaveBeenCalledWith(10);
+				done();
+			});
+	});
+
+	it('map from a stream with a water mark', (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy();
+
+		$C([0, 1, 2, 3, 4])
+			.to(new Transform({
+				readableObjectMode: true,
+				writableObjectMode: true,
+				highWaterMark: 2,
+				transform(chunk, enc, cb) {
+					setImmediate(() => cb(null, chunk));
+				}
+			}))
+
+			.map((el) => el * 2)
+			.addListener('data', dataHandler)
+			.addListener('end', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(length);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+				expect(dataHandler).toHaveBeenCalledWith(2);
+				expect(dataHandler).toHaveBeenCalledWith(4);
+				expect(dataHandler).toHaveBeenCalledWith(6);
+				expect(dataHandler).toHaveBeenCalledWith(8);
+				expect(dataHandler).not.toHaveBeenCalledWith(10);
+				done();
+			});
+	});
+
+	it('map from a stream with an error', (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy(),
+			errorHandler = jasmine.createSpy();
+
+		const
+			err = new Error('boom!');
+
+		$C(generateStream(length)).map((el, i) => {
+			if (i) {
+				throw err;
+			}
+
+			return el * 2;
+		})
+			.addListener('data', dataHandler)
+			.addListener('error', errorHandler)
+			.addListener('close', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(1);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+
+				expect(errorHandler).toHaveBeenCalledTimes(1);
+				expect(errorHandler).toHaveBeenCalledWith(err);
+				done();
+			});
+	});
+
+	it('map from a stream with a readable error', (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy(),
+			errorHandler = jasmine.createSpy();
+
+		const
+			err = new Error('boom!');
+
+		$C(generateStream(length, err)).map((el, i) => {
+			if (i) {
+				throw err;
+			}
+
+			return el * 2;
+		})
+			.addListener('data', dataHandler)
+			.addListener('error', errorHandler)
+			.addListener('close', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(1);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+
+				expect(errorHandler).toHaveBeenCalledTimes(1);
+				expect(errorHandler).toHaveBeenCalledWith(err);
+				done();
+			});
+	});
+
+	it('map from a stream with a writable error', (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy(),
+			errorHandler = jasmine.createSpy();
+
+		const
+			err = new Error('boom!');
+
+		let
+			i = 0;
+
+		$C(generateStream(length))
+			.to(new Transform({
+				readableObjectMode: true,
+				writableObjectMode: true,
+				highWaterMark: 2,
+				transform(chunk, enc, cb) {
+					if (i++) {
+						cb(err);
+						return;
+					}
+
+					setImmediate(() => cb(null, chunk));
+				}
+			}))
+
+			.map((el) => el * 2)
+
+			.addListener('data', dataHandler)
+			.addListener('error', errorHandler)
+			.addListener('close', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(1);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+
+				expect(errorHandler).toHaveBeenCalledTimes(1);
+				expect(errorHandler).toHaveBeenCalledWith(err);
+				done();
+			});
+	});
+
+	it('map from a stream with a write error', (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy(),
+			errorHandler = jasmine.createSpy();
+
+		const
+			err = new Error('boom!');
+
+		let
+			i = 0;
+
+		$C(generateStream(length))
+			.to(new Transform({
+				readableObjectMode: true,
+				writableObjectMode: true,
+				highWaterMark: 2,
+				transform(chunk, enc, cb) {
+					if (i++) {
+						throw err;
+					}
+
+					setImmediate(() => cb(null, chunk));
+				}
+			}))
+
+			.map((el) => el * 2)
+
+			.addListener('data', dataHandler)
+			.addListener('error', errorHandler)
+			.addListener('close', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(1);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+
+				expect(errorHandler).toHaveBeenCalledTimes(1);
+				expect(errorHandler).toHaveBeenCalledWith(err);
+				done();
+			});
+	});
+
+	it('map from a stream with closing', (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy(),
+			errorHandler = jasmine.createSpy();
+
+		let
+			i = 0;
+
+		$C(generateStream(length))
+			.to(new Transform({
+				readableObjectMode: true,
+				writableObjectMode: true,
+				highWaterMark: 2,
+				transform(chunk, enc, cb) {
+					if (i++) {
+						this.destroy();
+						return;
+					}
+
+					setImmediate(() => cb(null, chunk));
+				}
+			}))
+
+			.map((el) => el * 2)
+
+			.addListener('data', dataHandler)
+			.addListener('error', errorHandler)
+			.addListener('close', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(1);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+				expect(errorHandler).toHaveBeenCalledTimes(0);
+				done();
+			});
+	});
+
+	it('map from a stream with break', (done) => {
+		const
+			length = 5,
+			dataHandler = jasmine.createSpy();
+
+		$C(generateStream(length))
+			.map((el, i, data, o) => {
+				if (i) {
+					return o.break;
+				}
+
+				return el * 2;
+			})
+
+			.addListener('data', dataHandler)
+			.addListener('end', () => {
+				expect(dataHandler).toHaveBeenCalledTimes(1);
+				expect(dataHandler).toHaveBeenCalledWith(0);
+				done();
+			});
+	});
+
 	it('map from a write stream', (done) => {
 		const
 			length = 5,
 			dataHandler = jasmine.createSpy();
 
-		$C(generateStream(length, 'write')).map((el) => el * 2)
+		$C(generateStream(length, false, 'write')).map((el) => el * 2)
 			.addListener('data', dataHandler)
 			.addListener('end', () => {
 				expect(dataHandler).toHaveBeenCalledTimes(length);
