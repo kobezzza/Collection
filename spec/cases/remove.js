c321ebcd3ac4885e0006550ceac0a6c06d57076e
kobezzza <kobezzza@gmail.com>
2019-05-19 11:46:38 +0300

    Added more cases

--- spec/cases/remove.js
+++ spec/cases/remove.js
@@ -112,11 +112,61 @@ describe('Collection.remove', () => {
 		expect($C(obj).get(path)).toBeUndefined();
 	});
 
+	it('remove from an incorrect data type', () => {
+		function *data() {
+			for (let i = 0; i < 3; i++) {
+				yield i;
+			}
+		}
+
+		expect(() => $C(data).remove()).toThrowError('Incorrect data type');
+	});
+
 	it('remove from an array', () => {
 		const
 			obj = [1, 2, 3, 4, 5];
 
 		expect($C(obj).remove((el) => el % 2)).toEqual([
+			{
+				key: 0,
+				result: true,
+				value: 1
+			},
+
+			{
+				key: 2,
+				result: true,
+				value: 3
+			},
+
+			{
+				key: 4,
+				result: true,
+				value: 5
+			}
+		]);
+
+		expect(obj).toEqual([2, 4]);
+	});
+
+	it('single remove from an array', () => {
+		const
+			obj = [1, 2, 3, 4, 5];
+
+		expect($C(obj).one.remove((el) => el % 2)).toEqual({
+			key: 0,
+			result: true,
+			value: 1
+		});
+
+		expect(obj).toEqual([2, 3, 4, 5]);
+	});
+
+	it('async remove from an array', async () => {
+		const
+			obj = [1, 2, 3, 4, 5];
+
+		expect(await $C(obj).async.remove((el) => Promise.resolve(el % 2))).toEqual([
 			{
 				key: 0,
 				result: true,
@@ -166,6 +216,19 @@ describe('Collection.remove', () => {
 		expect(obj).toEqual([2, 4]);
 	});
 
+	it('single remove from a reversed array', () => {
+		const
+			obj = [1, 2, 3, 4, 5];
+
+		expect($C(obj).one.reverse.remove((el) => el % 2)).toEqual({
+			key: 4,
+			result: true,
+			value: 5
+		});
+
+		expect(obj).toEqual([1, 2, 3, 4]);
+	});
+
 	it('remove from a live array', () => {
 		const
 			obj = [1, 2, 3, 4, 5];
@@ -191,6 +254,139 @@ describe('Collection.remove', () => {
 		]);
 
 		expect(obj).toEqual([2, 4]);
+	});
+
+	it('single remove from a live array', () => {
+		const
+			obj = [1, 2, 3, 4, 5];
+
+		expect($C(obj).one.live.remove((el) => el % 2)).toEqual({
+			key: 0,
+			result: true,
+			value: 1
+		});
+
+		expect(obj).toEqual([2, 3, 4, 5]);
+	});
+
+	it('remove from array like', () => {
+		const
+			obj = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5};
+
+		expect($C(obj).remove((el) => el % 2)).toEqual([
+			{
+				key: 0,
+				result: true,
+				value: 1
+			},
+
+			{
+				key: 2,
+				result: true,
+				value: 3
+			},
+
+			{
+				key: 4,
+				result: true,
+				value: 5
+			}
+		]);
+
+		expect(obj).toEqual({0: 2, 1: 4, length: 2});
+	});
+
+	it('single remove from array like', () => {
+		const
+			obj = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5};
+
+		expect($C(obj).one.remove((el) => el % 2)).toEqual({
+			key: 0,
+			result: true,
+			value: 1
+		});
+
+		expect(obj).toEqual({0: 2, 1: 3, 2: 4, 3: 5, length: 4});
+	});
+
+	it('remove from reversed array like', () => {
+		const
+			obj = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5};
+
+		expect($C(obj).reverse.remove((el) => el % 2)).toEqual([
+			{
+				key: 4,
+				result: true,
+				value: 5
+			},
+
+			{
+				key: 2,
+				result: true,
+				value: 3
+			},
+
+			{
+				key: 0,
+				result: true,
+				value: 1
+			}
+		]);
+
+		expect(obj).toEqual({0: 2, 1: 4, length: 2});
+	});
+
+	it('single remove from reversed array like', () => {
+		const
+			obj = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5};
+
+		expect($C(obj).one.reverse.remove((el) => el % 2)).toEqual({
+			key: 4,
+			result: true,
+			value: 5
+		});
+
+		expect(obj).toEqual({0: 1, 1: 2, 2: 3, 3: 4, length: 4});
+	});
+
+	it('remove from live array like', () => {
+		const
+			obj = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5};
+
+		expect($C(obj).live.remove((el) => el % 2)).toEqual([
+			{
+				key: 0,
+				result: true,
+				value: 1
+			},
+
+			{
+				key: 2,
+				result: true,
+				value: 3
+			},
+
+			{
+				key: 4,
+				result: true,
+				value: 5
+			}
+		]);
+
+		expect(obj).toEqual({0: 2, 1: 4, length: 2});
+	});
+
+	it('single remove from live array like', () => {
+		const
+			obj = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5};
+
+		expect($C(obj).one.live.remove((el) => el % 2)).toEqual({
+			key: 0,
+			result: true,
+			value: 1
+		});
+
+		expect(obj).toEqual({0: 2, 1: 3, 2: 4, 3: 5, length: 4});
 	});
 
 	it('remove from an object', () => {
@@ -220,6 +416,19 @@ describe('Collection.remove', () => {
 		expect(obj).toEqual({b: 2, d: 4});
 	});
 
+	it('single remove from an object', () => {
+		const
+			obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+
+		expect($C(obj).one.remove((el) => el % 2)).toEqual({
+			key: 'a',
+			result: true,
+			value: 1
+		});
+
+		expect(obj).toEqual({b: 2, c: 3, d: 4, e: 5});
+	});
+
 	it('remove from a map', () => {
 		const
 			obj = new Map([[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]);
@@ -247,6 +456,19 @@ describe('Collection.remove', () => {
 		expect(obj).toEqual(new Map([[1, 2], [3, 4]]));
 	});
 
+	it('single remove from a map', () => {
+		const
+			obj = new Map([[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]);
+
+		expect($C(obj).one.remove((el) => el % 2)).toEqual({
+			key: 0,
+			result: true,
+			value: 1
+		});
+
+		expect(obj).toEqual(new Map([[1, 2], [2, 3], [3, 4], [4, 5]]));
+	});
+
 	it('remove from a set', () => {
 		const
 			obj = new Set([1, 2, 3, 4, 5]);
@@ -272,6 +494,19 @@ describe('Collection.remove', () => {
 		]);
 
 		expect(obj).toEqual(new Set([2, 4]));
+	});
+
+	it('single remove from a set', () => {
+		const
+			obj = new Set([1, 2, 3, 4, 5]);
+
+		expect($C(obj).one.remove((el) => el % 2)).toEqual({
+			key: null,
+			result: true,
+			value: 1
+		});
+
+		expect(obj).toEqual(new Set([2, 3, 4, 5]));
 	});
 
 	it('remove from an array without a filter', () => {
