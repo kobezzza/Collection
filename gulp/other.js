00e33d22e7effa90baf6ab77083b55d9b6234dc8
kobezzza <kobezzza@gmail.com>
2021-07-10 12:47:08 +0300

    Removed copyright task

--- gulp/other.js
+++ gulp/other.js
@@ -15,17 +15,6 @@ const
 	plumber = require('gulp-plumber'),
 	$ = require('gulp-load-plugins')();
 
-gulp.task('copyright', () =>
-	gulp.src('./LICENSE')
-		.pipe(plumber())
-		.pipe($.replace(/(Copyright \(c\) )(\d+)-?(\d*)/, (str, intro, from, to) => {
-			const year = new Date().getFullYear();
-			return intro + from + (to || from != year ? `-${year}` : '');
-		}))
-
-		.pipe(gulp.dest('./'))
-);
-
 gulp.task('bump', () =>
 	gulp.src('./@(package-lock|package).json')
 		.pipe(plumber())
